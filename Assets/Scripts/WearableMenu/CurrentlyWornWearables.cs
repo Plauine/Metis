5c82d72903acca69e1c80c6ceb1d95d56b8da99d
Plauine <paulinebaudouin2@gmail.com>
2019-09-28 16:49:00 +0200

    feat[Sliders]: Added stats bar on the right panel of UI
    Each slider represents a ModifierType and its valeu is updated accordingly to the currently worn items
    
    [Related Github Issue: #]

--- Assets/Scripts/WearableMenu/CurrentlyWornWearables.cs
+++ Assets/Scripts/WearableMenu/CurrentlyWornWearables.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Metis.ItemMenu
 {
     public static class CurrentlyWornWearables
     {
-        private static Dictionary<WearableType, GameObject> _currentlyWorn = new Dictionary<WearableType, GameObject>() {
+        private static Dictionary<WearableType, Wearable> _currentlyWorn = new Dictionary<WearableType, Wearable>() {
             { WearableType.HELMET, null},
             { WearableType.TOP, null},
             { WearableType.PANTS, null},
@@ -13,11 +14,36 @@ namespace Metis.ItemMenu
             { WearableType.WEAPON, null},
         };
 
+        public static Dictionary<ModifierType, float> _totalModifiers = new Dictionary<ModifierType, float>();
+
         public static void PutOnNewWearable(Wearable newWearable)
         {
-            _currentlyWorn[newWearable.Type]?.SetActive(false);
-            _currentlyWorn[newWearable.Type] = newWearable.WearableGameObject;
+            _currentlyWorn[newWearable.Type]?.WearableGameObject?.SetActive(false);
+            _currentlyWorn[newWearable.Type] = newWearable;
             newWearable.WearableGameObject?.SetActive(true);
+            UpdateTotalModifiers();
+            Stats.AllStatsUpdator.UpdateValues();
+        }
+
+        private static void UpdateTotalModifiers()
+        {
+            _totalModifiers.Clear();
+            foreach(var wearable in _currentlyWorn.Values)
+            {
+                if (wearable == null)
+                    continue;
+
+                foreach(Modifier modifier in wearable?.Modifiers)
+                {
+                    if (_totalModifiers.ContainsKey(modifier.Type))
+                    {
+                        _totalModifiers[modifier.Type] += modifier.Value;
+                    } else
+                    {
+                        _totalModifiers.Add(modifier.Type, modifier.Value);
+                    }
+                }
+            }
         }
     }
 }
