1187b2cf5737b4b79d586b74f3efaaff85fc5fe4
Plauine <paulinebaudouin2@gmail.com>
2019-10-12 17:28:15 +0200

    Created the new logic for the progress bars

--- Assets/Scripts/WearableMenu/StatBarsController.cs
+++ Assets/Scripts/WearableMenu/StatBarsController.cs
@@ -1,19 +1,48 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 
-public class StatBarsController : MonoBehaviour
+namespace Metis.Stats
 {
-    private readonly float maxValue = 100;
-    [SerializeField] private Image _statBar, _modifierBar;
-
-    private void Awake()
+    public class StatBarsController : MonoBehaviour
     {
-        SetupBar(50f, 3f);
-    }
+        [SerializeField]
+        private Slider _backSlider;
 
-    private void SetupBar(float currentValue, float modifierValue)
-    {
-        _statBar.fillAmount = currentValue / maxValue;
-        _modifierBar.fillAmount = modifierValue / maxValue;
+        [SerializeField]
+        private Slider _frontSlider;
+
+        [SerializeField]
+        private Image _backImage;
+
+        private float _currentValue;
+
+        private bool _backSliderNeedsUpdate, _frontSliderNeedsUpdate;
+
+        public void CheckAndUpdate(float newValue, bool isFirst)
+        {
+            newValue /= 100;
+
+            // if it is the initialisation
+            if (isFirst)
+            {
+                _frontSlider.value = 0;
+                _backSlider.value = newValue;
+                _currentValue = newValue;
+                _backImage.color = Color.white;
+                return;
+            }
+
+            // The back slider always takes the biggest value between the current and the new value
+            _backSlider.value = Mathf.Max(_currentValue, newValue);
+
+            // The fron slider take the smallest one
+            _frontSlider.value = Mathf.Min(_currentValue, newValue);
+
+            // The color of the back image depends on weather the current value is equal, strictly superior or inferior to the new value
+            _backImage.color = _currentValue == newValue ? Color.white : _currentValue > newValue ? Color.red : Color.green;
+
+            _backSliderNeedsUpdate = _currentValue != newValue;
+        }
     }
 }
