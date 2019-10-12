8faada808ebd9a0ad15b695494e0bde15c1b245e
Plauine <paulinebaudouin2@gmail.com>
2019-10-12 19:14:22 +0200

    animated the stats bars

--- Assets/Scripts/WearableMenu/StatBarsController.cs
+++ Assets/Scripts/WearableMenu/StatBarsController.cs
@@ -15,34 +15,59 @@ namespace Metis.Stats
         [SerializeField]
         private Image _backImage;
 
-        private float _currentValue;
+        private float _currentValue, _originalBackValue, _originalFrontValue, _nextBackValue, _nextFrontValue, _stepBackSlider, _stepFrontSlider, _speed = 2.5f;
 
         private bool _backSliderNeedsUpdate, _frontSliderNeedsUpdate;
 
         public void CheckAndUpdate(float newValue, bool isFirst)
         {
-            newValue /= 100;
-
             // if it is the initialisation
             if (isFirst)
             {
-                _frontSlider.value = 0;
-                _backSlider.value = newValue;
                 _currentValue = newValue;
-                _backImage.color = Color.white;
-                return;
             }
+            else
+                // The color of the back image depends on weather the current value is equal, strictly superior or inferior to the new value
+                _backImage.color = _currentValue > newValue ? Color.red : Color.green;
 
             // The back slider always takes the biggest value between the current and the new value
-            _backSlider.value = Mathf.Max(_currentValue, newValue);
+            _nextBackValue = isFirst ? newValue : Mathf.Max(_currentValue, newValue);
+            if(_nextBackValue != _backSlider.value)
+            {
+                _originalBackValue = _backSlider.value;
+                _backSliderNeedsUpdate = true;
+                _stepBackSlider = 0;
+            }
 
-            // The fron slider take the smallest one
-            _frontSlider.value = Mathf.Min(_currentValue, newValue);
+            _nextFrontValue = isFirst ? newValue : Mathf.Min(_currentValue, newValue);
+            if (_nextFrontValue != _frontSlider.value)
+            {
+                _originalFrontValue = _frontSlider.value;
+                _frontSliderNeedsUpdate = true;
+                _stepFrontSlider = 0;
+            }
 
-            // The color of the back image depends on weather the current value is equal, strictly superior or inferior to the new value
-            _backImage.color = _currentValue == newValue ? Color.white : _currentValue > newValue ? Color.red : Color.green;
+        }
 
-            _backSliderNeedsUpdate = _currentValue != newValue;
+        private void Update()
+        {
+            if (_backSliderNeedsUpdate)
+            {
+                _backSlider.value = Mathf.Lerp(_originalBackValue, _nextBackValue, _stepBackSlider);
+                _stepBackSlider += _speed * Time.deltaTime;
+
+                if (_backSlider.value == _nextBackValue)
+                    _backSliderNeedsUpdate = false;
+            }
+
+            if (_frontSliderNeedsUpdate)
+            {
+                _frontSlider.value = Mathf.Lerp(_originalFrontValue, _nextFrontValue, _stepFrontSlider);
+                _stepFrontSlider += _speed * Time.deltaTime;
+
+                if (_frontSlider.value == _nextFrontValue)
+                    _frontSliderNeedsUpdate = false;
+            }
         }
     }
 }
